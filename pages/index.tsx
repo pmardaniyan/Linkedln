5e654c6c607ce5b890b96d62cf7ff3191d803655
p.mardaniyan <p.mardaniyan@gmail.com>
2022-12-21 10:09:39 +0330

    Config Tailwindcss With NextJs13

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+      <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='text-red-400'>
+        LinjdIn Project Clone
+      </div>
+      </>
+    
+  )
+}
+
+export default Home
